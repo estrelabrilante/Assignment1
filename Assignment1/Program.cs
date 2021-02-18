692316e182c3e597a90a369f97cfeaf76dbd7ce3
unknown <shynumary@gmail.com>
2021-02-18 08:13:20 +0530

    completed rectangle class

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Assignment1;
+
 
 namespace Assignment1
 {
@@ -10,6 +12,37 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Rectangle Area and Parameter");
+            Console.WriteLine("Enter length of rectangle");// fetching parameter from user
+            int lengt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter width of rectagle");
+            int widt = Convert.ToInt32(Console.ReadLine());
+            if (lengt <= 0 || widt <= 0)
+            {
+                Console.WriteLine("Please enter proper value");
+
+            }
+
+
+            Rectangle cons = new Rectangle(lengt, widt); //instance created for non-default constructor
+            Rectangle x = new Rectangle(); // instance created for default constructor
+            Console.WriteLine("old length");
+            Console.WriteLine(x.GetLength());// getting default value
+            Console.WriteLine("old width");
+            Console.WriteLine(x.GetWidth());
+            Console.WriteLine("setting new value for length");
+            Console.WriteLine(cons.SetLength(lengt));
+            Console.WriteLine("setting new value for width");
+            Console.WriteLine(cons.SetWidth(widt));
+
+            cons.GetPerimeter();
+            cons.GetArea();
+            Console.Write("Perimeter:");
+            Console.WriteLine(cons.GetPerimeter());
+            Console.Write("Area:");
+            Console.WriteLine(cons.GetArea());
+
         }
     }
 }
---
+++ Assignment1/Rectangle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+   public class Rectangle
+    {
+
+        private int length, width; // private integer attributes
+        public Rectangle()// default constructor
+        {
+            length = 1; //set length and width as 1
+            width = 1;
+        }
+        public Rectangle(int lengt, int widt) //non default constructor
+        {
+            this.length = lengt;
+            this.width = widt;
+
+        }
+        public int GetLength()
+        {
+            return length;
+        }
+        public int SetLength(int length)
+        {
+
+            return length;
+        }
+        public int GetWidth()
+        {
+            return width;
+        }
+        public int SetWidth(int width)
+        {
+            return width;
+
+        }
+        public int GetPerimeter()
+        {
+            int Perimeter = 2 * (length + width);
+            return Perimeter;
+        }
+        public int GetArea()
+        {
+            int area = length * width;
+            return area;
+        }
+    }
+}
